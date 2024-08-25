94d3cc84183843c070bc3c2ed7086b01b76c1714
chidb2k3 <dangbachi345@gmail.com>
2024-08-25 18:49:42 +0700

    add all

--- obj/Debug/net8.0/exercises_week1.AssemblyInfo.cs
+++ obj/Debug/net8.0/exercises_week1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercises_week1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ae665ac6d4f2a071d02e710fd36511fdf06e253")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercises_week1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercises_week1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
